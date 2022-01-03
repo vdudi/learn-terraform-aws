a6e4a0c4f948647444af371b8721635de252b784
vdudi <vdudi@ncl.com>
2022-01-03 14:23:56 -0500

    AWS vpc, subnet creation

--- main.tf
+++ main.tf
@@ -12,12 +12,29 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+#resource "aws_instance" "example" {
+#  ami           = "ami-830c94e3"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "ExampleInstance"
+#  }
+#}
+
+resource "aws_vpc" "dev_main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "Dev"
   }
 }
 
+resource "aws_subnet" "subnet-1" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id     = aws_vpc.dev_main.id
+
+  tags = {
+    Name = "dev-subnet"
+  }
+}
